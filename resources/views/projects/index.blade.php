d4ec08fa3f89d226ae2bd77f6802c731df08e2c1
Sundeberg <1089441@ucn.dk>
2024-07-02 15:58:07 +0200

    remove modal view

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -222,7 +222,7 @@
                                 </div>
                             </div>
                             <!-- Edit Invoice Modal -->
-                            <div x-show="invoiceModalOpen" class="fixed inset-0 flex items-center justify-center z-50">
+                            {{-- <div x-show="invoiceModalOpen" class="fixed inset-0 flex items-center justify-center z-50">
                                 <div class="fixed inset-0 bg-black opacity-50"></div>
                                 <div class="fixed z-10 inset-0 overflow-y-auto pointer-events-none" id="editInvoiceModal" tabindex="-1" role="dialog" aria-labelledby="modal-title" aria-hidden="true">
                                     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 pointer-events-auto">
@@ -244,7 +244,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
