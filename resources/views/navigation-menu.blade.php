3abbd505ddfdb60aaf9419e6e8ffe81b0f4cc1d0
Sundebergius <1089441@ucn.dk>
2024-09-02 09:52:07 +0200

    Change to svg addition of nav bar, removal of upward arrow
    <path d="M19.5 8.25l-7.5 7.5-7.5-7.5M12 5.25l3.75 3.75L12 12.75"></path>

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -102,7 +102,10 @@
                                     {{ Auth::user()->name }}
 
                                     <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M19.5 8.25l-7.5 7.5-7.5-7.5M12 5.25l3.75 3.75L12 12.75" />
+                                        {{-- Old path model, trial to have upward as well as downward arrow --}}
+                                        {{-- <path d="M19.5 8.25l-7.5 7.5-7.5-7.5M12 5.25l3.75 3.75L12 12.75" /> --}}
+                                        
+                                        <path d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                     </svg>
                                 </button>
                             @endif
