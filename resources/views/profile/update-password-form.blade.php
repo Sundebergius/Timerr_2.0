b3dd1a42d92178e0efba39652f35a878c9c70c95
Sundebergius <1089441@ucn.dk>
2024-09-30 09:34:41 +0200

    update password with conditional check to see if password is null

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -8,21 +8,33 @@
     </x-slot>
 
     <x-slot name="form">
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="current_password" value="{{ __('Current Password') }}" />
-            <x-input id="current_password" type="password" class="mt-1 block w-full" wire:model="state.current_password" autocomplete="current-password" />
-            <x-input-error for="current_password" class="mt-2" />
-        </div>
+        @if (!is_null(auth()->user()->password))
+            <!-- Show current password field for users who already have a password -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="current_password" value="{{ __('Current Password') }}" />
+                <x-input id="current_password" type="password" class="mt-1 block w-full" wire:model.defer="state.current_password" autocomplete="current-password" />
+                <x-input-error for="current_password" class="mt-2" />
+            </div>
+        @else
+            <!-- Inform Google login users why they need to set a password -->
+            <div class="col-span-6 sm:col-span-4">
+                <p class="text-sm text-gray-600">
+                    {{ __('You are currently signed in using your Google account. To set a password for your account, please provide a new password below.') }}
+                </p>
+            </div>
+        @endif
 
+        <!-- New password field -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="password" value="{{ __('New Password') }}" />
-            <x-input id="password" type="password" class="mt-1 block w-full" wire:model="state.password" autocomplete="new-password" />
+            <x-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
             <x-input-error for="password" class="mt-2" />
         </div>
 
+        <!-- Confirm new password field -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-            <x-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model="state.password_confirmation" autocomplete="new-password" />
+            <x-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
             <x-input-error for="password_confirmation" class="mt-2" />
         </div>
     </x-slot>
