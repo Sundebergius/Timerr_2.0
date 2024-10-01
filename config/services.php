44ec6a4ad582bdabc738b84e346a1b2a4a101a79
Sundeberg <1089441@ucn.dk>
2024-10-01 19:41:48 +0200

    added stripe to services, proper load of secret keys

--- config/services.php
+++ config/services.php
@@ -41,6 +41,12 @@ return [
         'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
+    'stripe' => [
+        'secret' => env('STRIPE_SECRET'),
+        'key' => env('STRIPE_KEY'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+    ],
+
 
 
 ];
