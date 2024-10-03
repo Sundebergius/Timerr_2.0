10896cd09030682c76a2686e4d0a519f966ef73b
Sundeberg <1089441@ucn.dk>
2024-10-03 15:03:18 +0200

    added alpine to app.blade.php to show component messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,9 @@
             }
         </style>
 
+        <!-- Alpine.js for handling interactive UI components -->
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
         <!-- Livewire Global Error Handling -->
         <script>
             document.addEventListener('livewire:load', function () {
