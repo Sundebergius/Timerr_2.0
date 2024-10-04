bc036c420b198a44a3993cb47ef5a97d95b35a18
Sundeberg <1089441@ucn.dk>
2024-10-04 14:54:15 +0200

    visual improvement to no title

--- resources/js/components/TaskCreator.vue
+++ resources/js/components/TaskCreator.vue
@@ -8,12 +8,14 @@
         <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title:</label>
         <input ref="titleInput" type="text" id="title" v-model="task_title"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-          <div v-if="titleError" class="alert alert-danger alert-dismissible fade show" role="alert">
-            Please fill out this field.
-            <button type="button" class="close" @click="titleError = false" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
+          <div v-if="titleError" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Error:</strong>
+            <span class="block sm:inline">Please fill out this field.</span>
+            <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="titleError = false">
+              <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 7.066 5.652a1 1 0 10-1.414 1.414L8.586 10l-2.934 2.934a1 1 0 101.414 1.414L10 11.414l2.934 2.934a1 1 0 001.414-1.414L11.414 10l2.934-2.934a1 1 0 000-1.414z" clip-rule="evenodd"/></svg>
             </button>
-          </div>      
+          </div>
+      
       </div>
 
       <div class="mb-4">
